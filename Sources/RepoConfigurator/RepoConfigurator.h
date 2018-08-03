36b5e26e9d2873e272c863816a4c0ef9baff5fca
Maxim Khatskevich <maxim@khatskevi.ch>
2018-08-04 04:57:14 +0500

    Added basic project files

---
+++ Sources/RepoConfigurator/RepoConfigurator.h
@@ -0,0 +1,19 @@
+//
+//  RepoConfigurator.h
+//  RepoConfigurator
+//
+//  Created by Maxim Khatskevich on 2018-08-04.
+//  Copyright © 2018 XCEssentials. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RepoConfigurator.
+FOUNDATION_EXPORT double RepoConfiguratorVersionNumber;
+
+//! Project version string for RepoConfigurator.
+FOUNDATION_EXPORT const unsigned char RepoConfiguratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RepoConfigurator/PublicHeader.h>
+
+
