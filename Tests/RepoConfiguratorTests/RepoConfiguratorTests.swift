3e3d7fff8b20856b860eca96014302761ee5b916
Maxim Khatskevich <maxim@khatskevi.ch>
2018-09-04 02:33:42 +0500

    Improved unit tests to support SPM

--- Tests/RepoConfiguratorTests/RepoConfiguratorTests.swift
+++ Tests/RepoConfiguratorTests/RepoConfiguratorTests.swift
@@ -13,28 +13,20 @@ import XCERepoConfigurator
 
 //---
 
+final
 class RepoConfiguratorTests: XCTestCase
 {
+    static
+    var allTests = [
+        ("testSwiftLint", testSwiftLint)
+        ]
+
+    //---
+
     lazy
     var currentBundle: Bundle = .init(for: type(of: self))
 
     let targetFolder = URL.init(fileURLWithPath: "") // doesn't matter
-
-    func targetOutputFromResource(
-        named resourceName: String,
-        withExtension resourceExtension: String
-        ) -> String
-    {
-        let result = try? ExternalFile
-            .loadFromResource(
-                named: resourceName,
-                withExtension: resourceExtension
-            )
-
-        //---
-
-        return result ?? ""
-    }
 }
 
 //---
@@ -43,10 +35,37 @@ extension RepoConfiguratorTests
 {
     func testSwiftLint()
     {
-        let targetOutput = targetOutputFromResource(
-            named: ".swiftlint",
-            withExtension: "yml"
-        )
+        let targetOutput = """
+            # see docs at https://github.com/realm/SwiftLint
+
+            disabled_rules:
+              - function_parameter_count
+              - trailing_newline
+              - closure_parameter_position
+              - opening_brace
+              - nesting
+              - function_body_length
+              - file_length
+
+            # paths to ignore during linting. Takes precedence over 'included'.
+            excluded:
+              - Resources
+              - Carthage
+              - Pods
+              - Templates
+
+            # rules options:
+
+            line_length:
+              ignores_comments: true
+            trailing_whitespace:
+              ignores_empty_lines: true
+            type_name:
+              allowed_symbols: _
+            statement_position:
+              # https://github.com/realm/SwiftLint/issues/1181#issuecomment-272445593
+              statement_mode: uncuddled_else
+            """
 
         //---
 
