2e50cfe7a460fa5784d5c630c88a1a456c2743f3
Maxim Khatskevich <maxim@khatskevi.ch>
2018-09-07 01:27:29 +0500

    Added ‘Files’ as dependency
    we keep supporting both ‘Carthage’ and ‘SwiftPM’.

--- Package.swift
+++ Package.swift
@@ -6,22 +6,14 @@ import PackageDescription
 let package = Package(
     name: "XCERepoConfigurator",
     products: [
-        .library(name: "XCERepoConfigurator", targets: ["RepoConfigurator"])
+        .library(name: "XCERepoConfigurator", targets: ["RepoConfigurator"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JohnSundell/Files", from: "2.2.1"),
     ],
     targets: [
-        .target(
-            name: "RepoConfigurator",
-            dependencies: [
-                // none
-            ],
-            path: "./Sources/RepoConfigurator"
-        ),
-        .testTarget(
-            name: "RepoConfiguratorTests",
-            dependencies: [
-                "RepoConfigurator"
-            ]
-        )
+        .target(name: "RepoConfigurator", dependencies: ["Files"], path: "./Sources/RepoConfigurator"),
+        .testTarget(name: "RepoConfiguratorTests", dependencies: ["RepoConfigurator"]),
     ],
     swiftLanguageVersions: [.v4, .v4_2]
 )
