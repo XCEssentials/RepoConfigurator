6d810e49b8533dff1911e21a31a971b2422e4843
Maxim Khatskevich <maxim@khatskevi.ch>
2018-09-09 23:20:41 +0500

    Added SPM-based repo config implementation
    Implemented config scrip using pure SPM in a subfolder, because Marathon doesn’t support properly local dependencies.
    https://github.com/JohnSundell/Marathon/issues/185

---
+++ .setup/main.swift
@@ -0,0 +1,169 @@
+import Files
+
+import XCERepoConfigurator
+
+//---
+
+guard
+    let repoFolder = Folder.current.parent
+else
+{
+    preconditionFailure("❌ Expected to be in a subfolder, one level deep from root!")
+}
+
+print("✅ Repo folder: \(repoFolder.path)")
+
+//---
+
+guard
+    let companyName = repoFolder.parent?.name
+else
+{
+    preconditionFailure("❌ Expected to be two levels deep from a company-named folder!")
+}
+
+print("✅ Company name: \(companyName)")
+
+//---
+
+let productName = repoFolder.name
+
+print("✅ Product name (without company prefix): \(productName)")
+
+//---
+
+let projectName = productName
+
+let swiftVersion = "4.2"
+
+let authorName = "Maxim Khatskevich"
+
+let depTarget: DeploymentTarget = (.macOS, "10.13")
+
+//---
+
+guard
+    let fastlaneFolder = try? repoFolder
+        .createSubfolderIfNeeded(withName: Defaults.pathToFastlaneFolder)
+else
+{
+    preconditionFailure("❌ Failed to establish Fastlane base folder.")
+}
+
+print("✅ Fastlane folder: ./\(fastlaneFolder.name)")
+
+//---
+
+do
+{
+    print("Write '\(ReadMe.fileName)'")
+
+    let readMe = try ReadMe
+        .openSourceProduct(
+            header: [
+                .gitHubLicenseBadge(
+                    account: companyName,
+                    repo: productName
+                ),
+                .gitHubTagBadge(
+                    account: companyName,
+                    repo: productName
+                ),
+                .carthageCompatibleBadge(),
+                .swiftPMCompatibleBadge(),
+                .writtenInSwiftBadge(
+                    version: swiftVersion
+                )
+            ],
+            """
+
+            # \(productName)
+
+            """
+        )
+        .prepare(
+            targetFolder: repoFolder.path,
+            removeRepeatingEmptyLines: false
+        )
+        .writeToFileSystem(
+            ifFileExists: .skip
+        )
+
+    if
+        !readMe
+    {
+        print("ⓘ NOTE: skipped ReadMe, because it already exists.")
+    }
+
+    //---
+
+    print("Write '\(Git.RepoIgnore.fileName)'")
+
+    try Git
+        .RepoIgnore
+        .framework()
+        .prepare(
+            targetFolder: repoFolder.path
+        )
+        .writeToFileSystem()
+
+    //---
+
+    print("Write '\(SwiftLint.fileName)'")
+
+    try SwiftLint
+        .standard()
+        .prepare(
+            targetFolder: repoFolder.path
+        )
+        .writeToFileSystem()
+
+    //---
+
+    print("Write '\(License.MIT.fileName)'")
+
+    try License
+        .MIT(
+            copyrightYear: 2018,
+            copyrightEntity: authorName
+        )
+        .prepare(
+            targetFolder: repoFolder.path
+        )
+        .writeToFileSystem()
+
+    //---
+
+    print("Write '\(Fastlane.Fastfile.fileName)'")
+
+    try Fastlane
+        .Fastfile
+        .custom(
+            predefinedSections: [
+                .defaultHeader(),
+                .beforeRelease(
+                    projectName: projectName,
+                    cocoaPodsModuleName: nil
+                )
+            ]
+        )
+        .prepare(
+            targetFolder: fastlaneFolder.path
+        )
+        .writeToFileSystem()
+
+    //---
+
+    print("Write '\(GitHub.PagesConfig.fileName)'")
+
+    try GitHub
+        .PagesConfig()
+        .prepare(
+            targetFolder: repoFolder.path
+        )
+        .writeToFileSystem()
+}
+catch
+{
+    print(error)
+}
