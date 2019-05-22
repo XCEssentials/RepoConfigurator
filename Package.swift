bea050335649b335df10fc74f2f47e8771ceb0c2
Maxim Khatskevich <maxim@khatskevi.ch>
2019-05-22 19:55:14 +0200

    [UPGRADE] own setup script
    also improved project structure and naming conventions.

--- Package.swift
+++ Package.swift
@@ -1,22 +1,40 @@
 // swift-tools-version:4.2
-// Managed by ice
 
 import PackageDescription
 
 let package = Package(
     name: "XCERepoConfigurator",
     products: [
-        .library(name: "XCERepoConfigurator", targets: ["XCERepoConfigurator"]),
+        .library(
+            name: "XCERepoConfigurator",
+            targets: [
+                "XCERepoConfigurator"
+            ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
         .package(url: "https://github.com/nvzqz/FileKit", from: "5.2.0"),
-        .package(url: "https://github.com/nschum/SwiftHamcrest", from: "2.1.0"),
+        .package(url: "https://github.com/nschum/SwiftHamcrest", .exact("2.1.1")),
         .package(url: "https://github.com/mxcl/Version.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "XCERepoConfigurator", dependencies: ["Version", "FileKit"], path: "./Sources/RepoConfigurator"),
-        .testTarget(name: "RepoConfiguratorTests", dependencies: ["XCERepoConfigurator", "Version", "SwiftHamcrest"]),
+        .target(
+            name: "XCERepoConfigurator",
+            dependencies: [
+                "Version",
+                "FileKit"
+            ],
+            path: "Sources/Core"
+        ),
+        .testTarget(
+            name: "XCERepoConfiguratorAllTests",
+            dependencies: [
+                "XCERepoConfigurator",
+                "Version",
+                "SwiftHamcrest"
+            ],
+            path: "Tests/AllTests"
+        ),
     ],
     swiftLanguageVersions: [.v4, .v4_2]
 )
