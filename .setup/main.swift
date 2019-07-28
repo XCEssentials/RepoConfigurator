379f3b08ea525dfd599b13e66136b7b3c025fa8d
Maxim Khatskevich <maxim@khatskevi.ch>
2019-07-28 16:09:58 +0200

    [UPGRADE] setup script

--- .setup/main.swift
+++ .setup/main.swift
@@ -11,15 +11,18 @@ print("--- BEGIN of '\(Executable.name)' script ---")
 
 // MARK: Parameters
 
-Spec.BuildSettings.swiftVersion.value = "4.2"
-let swiftLanguageVersionsForSPM = "[.v4, .v4_2]"
+Spec.BuildSettings.swiftVersion.value = "5.0"
+let swiftLangVersions = "[.v5]"
 
-let remoteRepo = try Spec.RemoteRepo()
+let localRepo = try Spec.LocalRepo.current()
 
-let travis = (
-    server: "https://travis-ci.com",
-    account: remoteRepo.accountName,
-    repo: remoteRepo.name,
+let remoteRepo = try Spec.RemoteRepo(
+    accountName: localRepo.context,
+    name: localRepo.name
+)
+
+let travisCI = (
+    address: "https://travis-ci.com/\(remoteRepo.accountName)/\(remoteRepo.name)",
     branch: "master"
 )
 
@@ -28,69 +31,46 @@ let company = try Spec.Company(
     identifier: "com.\(remoteRepo.accountName)"
 )
 
-let project = try Spec.Project(
+let project = (
+    name: remoteRepo.name,
     summary: "Generate repo config files using Swift and Xcode.",
-    copyrightYear: 2018,
-    deploymentTargets: [
-        .macOS : "10.11"
-    ]
+    copyrightYear: 2018
 )
 
-let cocoaPod = try Spec.CocoaPod(
-    companyInfo: .from(company),
-    productInfo: .from(project),
-    authors: [
-        ("Maxim Khatskevich", "maxim@khatskevi.ch")
-    ]
+let productName = company.prefix + project.name
+
+let authors = [
+    ("Maxim Khatskevich", "maxim@khatskevi.ch")
+]
+
+let desktop: DeploymentTarget = (.macOS, "10.11")
+
+typealias PerSubSpec<T> = (
+    core: T,
+    tests: T
 )
 
-let desktop = project.deploymentTargets[0]
-
-let subSpecs = (
-    core: Spec.CocoaPod.SubSpec(
-        "", // will pe extended with product name prefix
-        sourcesLocation: Spec.Locations.sources + ["Core"]
-    ),
-    tests: Spec.CocoaPod.SubSpec.tests()
+let subSpecs: PerSubSpec = (
+    "Core",
+    "AllTests"
 )
 
-let allSubspecs = try Spec
-    .CocoaPod
-    .SubSpec
-    .extractAll(from: subSpecs)
+let targetNames: PerSubSpec = (
+    productName,
+    productName + subSpecs.tests
+)
 
-let targetsSPM = (
-    core: (
-        productName: cocoaPod.product.name,
-        name: cocoaPod.product.name + subSpecs.core.name
-    ),
-    allTests: (
-        name: cocoaPod.product.name + subSpecs.tests.name,
-        none: ()
-    )
+let sourcesLocations: PerSubSpec = (
+    Spec.Locations.sources + subSpecs.core,
+    Spec.Locations.tests + subSpecs.tests
 )
 
 // MARK: Parameters - Summary
 
 remoteRepo.report()
 company.report()
-project.report()
-cocoaPod.report()
 
 // MARK: -
-
-// MARK: Write - Bundler - Gemfile
-
-try Bundler
-    .Gemfile(
-        basicFastlane: true,
-        """
-        gem '\(CocoaPods.gemName)'
-        gem '\(CocoaPods.Rome.gemName)'
-        """
-    )
-    .prepare()
-    .writeToFileSystem()
 
 // MARK: Write - ReadMe
 
@@ -108,13 +88,22 @@ try ReadMe()
     .addWrittenInSwiftBadge(
         version: Spec.BuildSettings.swiftVersion.value
     )
+    .addStaticShieldsBadge(
+        "platforms",
+        status: "macOS | Linux",
+        color: "blue",
+        title: "Supported platforms",
+        link: "Package.swift"
+    )
     .add("""
-        [![Build Status](\(travis.server)/\(travis.account)/\(travis.repo).svg?branch=\(travis.branch))](\(travis.server)/\(travis.account)/\(travis.repo))
+        [![Build Status](\(travisCI.address).svg?branch=\(travisCI.branch))](\(travisCI.address))
         """
     )
     .add("""
 
         # \(project.name)
+
+        \(project.summary)
 
         """
     )
@@ -122,84 +111,15 @@ try ReadMe()
         removeRepeatingEmptyLines: false
     )
     .writeToFileSystem(
-        ifFileExists: .skip // ONLY write if missing!
+        ifFileExists: .skip
     )
-
-// MARK: Write - SwiftLint
-
-try allSubspecs
-    .filter{ !$0.tests }
-    .map{ $0.linterCfgLocation }
-    .forEach{
-        
-        try SwiftLint
-            .standard()
-            .prepare(
-                at: $0
-            )
-            .writeToFileSystem()
-    }
 
 // MARK: Write - License
 
 try License
     .MIT(
-        copyrightYear: project.copyrightYear,
-        copyrightEntity: cocoaPod.authors[0].name
-    )
-    .prepare()
-    .writeToFileSystem()
-
-// MARK: Write - CocoaPods - Podfile
-
-try CocoaPods
-    .Podfile()
-    .custom("""
-        platform :\(desktop.platform.cocoaPodsId), '\(desktop.minimumVersion)'
-
-        plugin '\(CocoaPods.Rome.gemName)'
-
-        target 'Abstract' do
-
-            pod 'SwiftLint'
-
-        end
-        """
-    )
-    .prepare()
-    .writeToFileSystem()
-
-// MARK: Write - Fastlane - Fastfile
-
-try Fastlane
-    .Fastfile
-    .ForLibrary()
-    .defaultHeader()
-    .generateProjectViaSwiftPM(
-        for: cocoaPod,
-        scriptBuildPhases: {
-            
-            try $0.swiftLint(
-                project: [cocoaPod.product.name],
-                targetNames: [targetsSPM.core.name],
-                params:[
-                    """
-                    --path "\(subSpecs.core.sourcesLocation)"
-                    """
-                ]
-            )
-        },
-        endingEntries: [
-            
-            """
-            
-            # default initial location for any command
-            # is inside 'Fastlane' folder
-
-            sh 'cd ./.. && bundle exec pod install'
-
-            """
-        ]
+        copyrightYear: UInt(project.copyrightYear),
+        copyrightEntity: authors.map{ $0.0 }.joined(separator: ", ")
     )
     .prepare()
     .writeToFileSystem()
@@ -214,15 +134,16 @@ try GitHub
 // MARK: Write - Git - .gitignore
 
 try Git
-    .RepoIgnore
-    .framework(
-        otherEntries: [
-            """
-            # we don't need to store any project files,
-            # as we generate them on-demand from specs
-            *.\(Xcode.Project.extension)
-            """
-        ]
+    .RepoIgnore()
+    .addMacOSSection()
+    .addCocoaSection()
+    .addSwiftPackageManagerSection(ignoreSources: true)
+    .add(
+        """
+        # we don't need to store project file,
+        # as we generate it on-demand
+        *.\(Xcode.Project.extension)
+        """
     )
     .prepare()
     .writeToFileSystem()
@@ -235,89 +156,46 @@ try CustomTextFile("""
     import PackageDescription
 
     let package = Package(
-        name: "\(cocoaPod.product.name)",
+        name: "\(productName)",
         products: [
             .library(
-                name: "\(targetsSPM.core.productName)",
+                name: "\(productName)",
                 targets: [
-                    "\(targetsSPM.core.name)"
+                    "\(targetNames.core)"
                 ]
             )
         ],
         dependencies: [
             .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-            .package(url: "https://github.com/nschum/SwiftHamcrest", .exact("2.1.1")),
-            .package(url: "https://github.com/mxcl/Version.git", from: "1.0.0")
+            .package(url: "https://github.com/mxcl/Version.git", from: "1.0.0"),
+            .package(url: "https://github.com/nschum/SwiftHamcrest", from: "2.2.1")
         ],
         targets: [
             .target(
-                name: "\(targetsSPM.core.name)",
+                name: "\(targetNames.core)",
                 dependencies: [
                     "Version",
                     "PathKit"
                 ],
-                path: "\(subSpecs.core.sourcesLocation)"
+                path: "\(sourcesLocations.core)"
             ),
             .testTarget(
-                name: "\(targetsSPM.allTests.name)",
+                name: "\(targetNames.tests)",
                 dependencies: [
-                    "\(targetsSPM.core.name)",
+                    "\(targetNames.core)",
                     "Version",
                     "SwiftHamcrest"
                 ],
-                path: "\(subSpecs.tests.sourcesLocation)"
+                path: "\(sourcesLocations.tests)"
             ),
         ],
-        swiftLanguageVersions: \(swiftLanguageVersionsForSPM)
+        swiftLanguageVersions: \(swiftLangVersions)
     )
     """
     )
-    .prepare(at: ["Package.swift"])
-    .writeToFileSystem()
-
-// MARK: Write - .travis.yml
-
-try CustomTextFile("""
-    # https://docs.travis-ci.com/user/customizing-the-build/
-    # https://docs.travis-ci.com/user/job-lifecycle/#the-job-lifecycle
-    # https://docs.travis-ci.com/user/languages/objective-c/
-    
-    branches:
-      only:
-      - master
-      - /^hotfix.*$/
-      - /^release.*$/
-      - /^feature.*$/
-    
-    git:
-      depth: 3
-      submodules: false
-
-    language: objective-c
-
-    osx_image:
-      - xcode10.2
-      - xcode10.1
-
-    before_install:
-     - bundle install --path .vendor/bundle --jobs=3 --retry=3 --deployment
-     - bundle exec pod repo update
-
-    install:
-     - bundle exec pod install
-
-    before_script:
-      # cd ./.setup && swift run && cd ./.. # RUN this manually!
-      - bundle exec fastlane generateProjectViaSwiftPM
-      - swift --version
-
-    xcode_project: \(cocoaPod.product.name).\(Xcode.Project.extension)
-    xcode_scheme: \(cocoaPod.product.name)-Package
-    xcode_destination: platform=macOS
-    
-    """
+    .prepare(
+        at: ["Package.swift"]
     )
-    .prepare(at: [".travis.yml"])
     .writeToFileSystem()
 
 // MARK: - POST-script invocation output
